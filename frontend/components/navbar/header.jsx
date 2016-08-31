a336998656316ad08543933d3f6a6862df9fdec2
Daniel St. Clair <stclair.daniel@gmail.com>
2016-08-30 21:45:25 -0700

    made logo a link to home

--- frontend/components/navbar/header.jsx
+++ frontend/components/navbar/header.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import SessionNavContainer from '../session/session_nav_container';
+import { withRouter } from 'react-router';
 
-const Header = () => (
+class Header extends React.Component {
+
+  render () {
+  return (
   <header className="navbar">
-    <div className="nav-logo">
+    <div className="nav-logo" onClick={() => {this.props.router.push('/');}}>
       <img className="nav-img" src="assets/logo.png"/>
       <h1 className="nav-h1">Loud Sounds</h1>
     </div>
     <SessionNavContainer />
   </header>
-);
+    );
+  }
+}
 
-export default Header;
+export default withRouter(Header);
