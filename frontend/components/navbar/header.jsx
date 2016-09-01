75b479d0fa9aa59e309d458cb1d283b3ec176a7f
Daniel St. Clair <stclair.daniel@gmail.com>
2016-09-01 09:19:56 -0700

    changed logo hosting to cloudinary

--- frontend/components/navbar/header.jsx
+++ frontend/components/navbar/header.jsx
@@ -8,7 +8,7 @@ class Header extends React.Component {
   return (
   <header className="navbar">
     <div className="nav-logo" onClick={() => {this.props.router.push('/');}}>
-      <img className="nav-img" src="assets/logo.png"/>
+      <img className="nav-img" src="http://res.cloudinary.com/loudsounds/image/upload/c_scale,h_45,w_45/v1472745798/logo_yorfu3.png"/>
       <h1 className="nav-h1">Loud Sounds</h1>
     </div>
     <SessionNavContainer />
