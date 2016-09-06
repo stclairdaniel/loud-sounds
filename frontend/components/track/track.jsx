1de75e9cb97f65cc6dd03070ca9e4e9a4e0a4d74
Daniel St. Clair <stclair.daniel@gmail.com>
2016-09-05 20:43:37 -0700

    added comment counter and icon

--- frontend/components/track/track.jsx
+++ frontend/components/track/track.jsx
@@ -109,6 +109,7 @@ class Track extends React.Component {
     }
   }
 
+
   render () {
     return (
       <div className='track-container'>
@@ -146,8 +147,8 @@ class Track extends React.Component {
               <img src="http://res.cloudinary.com/loudsounds/image/upload/c_scale,w_25/v1472928619/pencil-512_ddms2g.png" className={this.showIcon()} onClick={this.editClickHandler}></img>
               <img src={this.likeIcon()} onClick={this.likeClickHandler}></img>
               <span>{this.props.tracks[this.props.track.id].likes.length}</span>
+              <img src="http://res.cloudinary.com/loudsounds/image/upload/c_scale,w_25/v1473133152/comment_icon2_t7epjf.png" onClick={this.trackClickHandler}></img>
               <span>{this.props.tracks[this.props.track.id].comments.length}</span>
-              <span>Comments</span>
             </div>
           </div>
         </div>
